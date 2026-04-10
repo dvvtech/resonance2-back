1d22e6422e9032dd6c801b5acdadb68945f4c25e
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-10 07:35:58 +0300

    set PlayLeadTimeMs to 1500

--- src/Resonance.Api/Services/RoomSyncServiceV2.cs
+++ src/Resonance.Api/Services/RoomSyncServiceV2.cs
@@ -6,7 +6,7 @@ namespace Resonance.Api.Services;
 
 public sealed class RoomSyncServiceV2
 {
-    private const int PlayLeadTimeMs = 3000;
+    private const int PlayLeadTimeMs = 1500;
     private const int SeekLeadTimeMs = 1000;
 
     private readonly ConcurrentDictionary<string, RoomV2> _rooms = new(StringComparer.OrdinalIgnoreCase);
